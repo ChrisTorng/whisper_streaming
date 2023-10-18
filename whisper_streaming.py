ebb66c3ae8424ddcdf053650c5f728ce75b3c6cb
ChrisTorng <ChrisTorng@hotmail.com>
2023-10-19 00:03:01 +0800

    Add whisper_online_stripped.py. whisper_streaming.py use OnlineASRProcessor but not success.

--- whisper_streaming.py
+++ whisper_streaming.py
@@ -23,15 +23,15 @@ RATE = 16000 # 8000, 16000, 32000
 FRAMES_PER_BUFFER = RATE * 5 # 320
 
 src_lan = "zh"  # source language
-# tgt_lan = "en"  # target language  -- same as source for ASR, "en" if translate task is used
+tgt_lan = "zh"  # target language  -- same as source for ASR, "en" if translate task is used
 
 printt("loading")
 asr = FasterWhisperASR(src_lan, "small")  # loads and wraps Whisper model
 # set options:
 # asr.set_translate_task()  # it will translate from lan into English
-asr.use_vad()  # set using VAD 
+# asr.use_vad()  # set using VAD 
 
-# online = OnlineASRProcessor(tgt_lan, asr)  # create processing object
+online = OnlineASRProcessor(asr, create_tokenizer(tgt_lan))  # create processing object
 
 pa = pyaudio.PyAudio()
 stream = pa.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=FRAMES_PER_BUFFER)
@@ -50,15 +50,15 @@ while True:   # processing loop:
 	# wf.close()
 	# textlist = asr.transcribe(wf, init_prompt="繁體中文台灣用語")
 
-	audio_recorded_filename = f'audio/temp/RECORDED-{str(time.time())}.wav'
-	wf = wave.open(audio_recorded_filename, 'wb')
-	wf.setnchannels(CHANNELS)
-	wf.setsampwidth(pa.get_sample_size(FORMAT))
-	wf.setframerate(RATE)
-	wf.writeframes(audio_frames)
-	wf.close()
-	printt("saved")
-	textlist = asr.transcribe(audio_recorded_filename, init_prompt="繁體中文台灣用語")
+	# audio_recorded_filename = f'audio/temp/RECORDED-{str(time.time())}.wav'
+	# wf = wave.open(audio_recorded_filename, 'wb')
+	# wf.setnchannels(CHANNELS)
+	# wf.setsampwidth(pa.get_sample_size(FORMAT))
+	# wf.setframerate(RATE)
+	# wf.writeframes(audio_frames)
+	# wf.close()
+	# printt("saved")
+	# textlist = asr.transcribe(audio_recorded_filename, init_prompt="繁體中文台灣用語")
 
 	# audio_data = np.ndarray(buffer=audio_frames, dtype=np.int16, shape=(FRAMES_PER_BUFFER, ))
 	# audio_data = np.frombuffer(audio_frames, dtype=np.int16)
@@ -67,15 +67,17 @@ while True:   # processing loop:
 	# audio_stream = io.BytesIO(audio_frames)
 	# textlist = asr.transcribe(audio_stream, init_prompt="繁體中文台灣用語")
 	
-	for segment in textlist:
-		printt(segment.text)
+	# for segment in textlist:
+	# 	printt(segment.text)
 	# print(textlist)
-	# online.insert_audio_chunk(a)
-	# o = online.process_iter()
-	# print(o) # do something with current partial output
+
+	online.insert_audio_chunk(audio_frames)
+	o = online.process_iter()
+	print(o) # do something with current partial output
+
 # at the end of this audio processing
-# o = online.finish()
-# print(o)  # do something with the last output
+o = online.finish()
+print(o)  # do something with the last output
 
 
-# online.init()  # refresh if you're going to re-use the object for the next audio
+online.init()  # refresh if you're going to re-use the object for the next audio
